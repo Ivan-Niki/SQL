e382d9b25669acdb340e576df0e121ae53bc0c57
Ivan-Niki <iFankov@gmail.com>
2023-09-20 23:48:25 +0300

    add timediff-function example 42-task

--- sql-academy-trainer.sql
+++ sql-academy-trainer.sql
@@ -312,6 +312,13 @@ WHERE last_name = 'Romashkin';
 SELECT start_pair FROM Timepair
 WHERE id = 4
 
+/* Задание 42
+Сколько времени обучающийся будет находиться в школе, учась со 2-го по 4-ый уч. предмет? */
+SELECT DISTINCT TIMEDIFF(
+                (SELECT end_pair FROM Timepair WHERE id = 4),
+                (SELECT start_pair FROM Timepair WHERE id = 2)
+                ) AS time
+FROM Timepair;
 
 
 
