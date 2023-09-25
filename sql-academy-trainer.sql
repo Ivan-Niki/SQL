103880cdace1284b806e3f3318f5469266968eb8
Ivan-Niki <iFankov@gmail.com>
2023-09-26 02:39:55 +0300

    add WEEK- and YEAR-functions example 61-task

--- sql-academy-trainer.sql
+++ sql-academy-trainer.sql
@@ -452,5 +452,12 @@ WHERE phone_number LIKE '+375%';
 
 
 
+/* Задание 61
+Выведите список комнат, которые были зарезервированы в течение 12 недели 2020 года. */
+SELECT DISTINCT Rooms.* FROM Rooms
+JOIN Reservations
+    ON Rooms.id = Reservations.room_id
+WHERE WEEK(start_date, 1) = 12 AND YEAR(start_date) = 2020;
+
 
 
