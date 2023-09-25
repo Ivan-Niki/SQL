7a7b47d24f2bf2a542794eecd1d2d9d71f511aa3
Ivan-Niki <iFankov@gmail.com>
2023-09-25 17:33:15 +0300

    add HW2 roles table tasks 7-9

--- HW2_sql_36gr.sql
+++ HW2_sql_36gr.sql
@@ -188,3 +188,46 @@
 		
 
 --------------------------------------------------------------------------------------------
+------- Таблица roles -------
+
+/* 7. Создать таблицу roles
+- id. Serial  primary key,
+- role_name. int, not null, unique */
+	
+	 CREATE TABLE roles (
+		id SERIAL PRIMARY KEY,
+		role_name INT NOT NULL UNIQUE
+	);
+
+	
+/* 8. Поменять тип столба role_name с int на varchar(30) */
+
+	ALTER TABLE roles ALTER COLUMN role_name TYPE VARCHAR(30);
+	
+	
+/* 9. Наполнить таблицу roles 20 строками: */
+
+	INSERT INTO roles (role_name)
+	VALUES  ('Junior Python developer'),
+			('Middle Python developer'),
+			('Senior Python developer'),
+			('Junior Java developer'),
+			('Middle Java developer'),
+			('Senior Java developer'),
+			('Junior JavaScript developer'),
+			('Middle JavaScript developer'),
+			('Senior JavaScript developer'),
+			('Junior Manual QA engineer'),
+			('Middle Manual QA engineer'),
+			('Senior Manual QA engineer'),
+			('Project Manager'),
+			('Designer'),
+			('HR'),
+			('CEO'),
+			('Sales manager'),
+			('Junior Automation QA engineer'),
+			('Middle Automation QA engineer'),
+			('Senior Automation QA engineer');
+			
+		
+--------------------------------------------------------------------------------------------
