364af7cf0873b01d1a24852ee577900d2bbd4dd0
Ivan-Niki <iFankov@gmail.com>
2023-09-25 23:40:51 +0300

    add HW3 left joins tasks 3-4

--- HW3_sql_36gr.sql
+++ HW3_sql_36gr.sql
@@ -16,3 +16,23 @@ JOIN employee_salary
 JOIN salary
 	ON employee_salary.salary_id = salary.id
 WHERE monthly_salary < 2000;
+
+
+--- 3. Вывести все зарплатные позиции, но работник по ним не назначен. (ЗП есть, но не понятно кто её получает.)
+SELECT salary_id, employee_name, monthly_salary
+FROM employee_salary
+LEFT JOIN employees
+	ON employee_salary.employee_id = employees.id
+LEFT JOIN salary
+	ON employee_salary.salary_id = salary.id
+--WHERE employee_name is NULL;
+	
+/* 4. Вывести все зарплатные позиции  меньше 2000 но работник по ним не назначен. (ЗП есть, но не понятно кто её получает.) */
+SELECT salary_id, employee_name, monthly_salary
+FROM employee_salary
+LEFT JOIN employees
+	ON employee_salary.employee_id = employees.id
+LEFT JOIN salary
+	ON employee_salary.salary_id = salary.id
+WHERE monthly_salary < 2000;
+--WHERE employee_name is NULL;
