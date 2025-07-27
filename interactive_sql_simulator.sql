7617cc959714c3647779c2cfce1b6224be405335
Ivan-Niki <iFankov@gmail.com>
2025-07-27 22:46:08 +0300

    add task-7-8 WHERE-operator

--- interactive_sql_simulator.sql
+++ interactive_sql_simulator.sql
@@ -47,3 +47,17 @@ SELECT author, title,
          IF(author = 'Есенин С.А.', price * 1.05, price * 1.0)),
         2) AS new_price
 FROM book;
+
+-- 7) Вывести автора, название и цены тех книг, количество которых меньше 10.
+SELECT author, title, price
+FROM book
+WHERE amount < 10;
+
+/* 8) Вывести название, автора,  цену  и количество всех книг, цена которых меньше 500 или 
+больше 600, а стоимость всех экземпляров этих книг больше или равна 5000. */
+SELECT title, author, price, amount
+FROM book
+WHERE (price < 500 OR price > 600) AND price * amount >= 5000;
+
+
+
