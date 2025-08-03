5078b8cc70fc9290e2732948ffcefe0698067b3b
Ivan-Niki <iFankov@gmail.com>
2025-08-03 23:27:12 +0300

    added task 26 creating an empty table

--- interactive_sql_simulator.sql
+++ interactive_sql_simulator.sql
@@ -229,3 +229,26 @@ WHERE amount IN (
     HAVING COUNT(amount) = 1
     );
 	
+
+
+
+
+
+-- ================= Создание пустой таблицы =================== --
+-- 26) Создать таблицу поставок (supply), которая имеет ту же структуру, что и таблиц book.
+
+Поле:	    Тип, описание:
+supply_id	INT PRIMARY KEY AUTO_INCREMENT
+title	    VARCHAR(50)
+author	    VARCHAR(30)
+price	    DECIMAL(8, 2)
+amount	    INT
+
+CREATE TABLE supply(
+    supply_id INT PRIMARY KEY AUTO_INCREMENT, 
+    title VARCHAR(50),
+    author VARCHAR(30),
+    price DECIMAL(8, 2),
+    amount INT
+);
+
