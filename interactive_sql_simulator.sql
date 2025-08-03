8553f894c12565f3fea12d30409f6d36993f7c49
Ivan-Niki <iFankov@gmail.com>
2025-08-03 23:19:25 +0300

    added task 21 subqueries in expressions

--- interactive_sql_simulator.sql
+++ interactive_sql_simulator.sql
@@ -211,3 +211,9 @@ FROM book
 WHERE price <= (SELECT AVG(price) FROM book)
 ORDER BY price DESC;
 
+/* 21) Вывести информацию (автора, название и цену) о тех книгах, цены которых превышают 
+минимальную цену книги на складе не более чем на 150 рублей в отсортированном по возрастанию цены виде. */
+SELECT author, title, price
+FROM book
+WHERE (price - (SELECT MIN(price) from book)) <= 150
+ORDER BY price;
