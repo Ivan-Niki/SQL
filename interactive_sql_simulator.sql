dfaa462eed3b2f2de7aa059eed534fe44996cfdb
Ivan-Niki <iFankov@gmail.com>
2025-08-04 01:25:02 +0300

    added task 32 multiple-table-update-requests

--- interactive_sql_simulator.sql
+++ interactive_sql_simulator.sql
@@ -299,3 +299,11 @@ WHERE amount BETWEEN 5 AND 10;
 UPDATE book SET buy = amount WHERE buy > amount;
 UPDATE book SET price = price * 0.9 WHERE buy = 0;
 
+/* 32) Для тех книг в таблице book , которые есть в таблице supply, не только увеличить их 
+количество в таблице book (увеличить их количество на значение столбца amount таблицы supply), 
+но и пересчитать их цену (для каждой книги найти сумму цен из таблиц book и supply и разделить на 2). */
+UPDATE book, supply 
+SET book.amount = book.amount + supply.amount,
+book.price = (book.price + supply.price)/2
+WHERE book.title = supply.title AND book.author = supply.author;
+
