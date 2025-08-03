6912cf1b68cc70139c97b27edfdcf67416af09d0
Ivan-Niki <iFankov@gmail.com>
2025-08-03 23:21:18 +0300

    added task 22 subqueries and IN-operator

--- interactive_sql_simulator.sql
+++ interactive_sql_simulator.sql
@@ -217,3 +217,15 @@ SELECT author, title, price
 FROM book
 WHERE (price - (SELECT MIN(price) from book)) <= 150
 ORDER BY price;
+
+/* 22) Вывести информацию (автора, книгу и количество) о тех книгах, количество экземпляров 
+которых в таблице book не дублируется. */
+SELECT author, title, amount
+FROM book
+WHERE amount IN (
+    SELECT amount 
+    FROM book 
+    GROUP BY amount 
+    HAVING COUNT(amount) = 1
+    );
+	
