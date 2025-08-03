a30dde1284e9a9d35d58f2a1c4d3ec8888bbadcf
Ivan-Niki <iFankov@gmail.com>
2025-08-04 01:09:13 +0300

    added task 28-30 updating several columns

--- interactive_sql_simulator.sql
+++ interactive_sql_simulator.sql
@@ -291,3 +291,11 @@ WHERE author NOT IN (
 UPDATE book 
 SET price = 0.9 * price
 WHERE amount BETWEEN 5 AND 10;
+
+
+/* 31) В таблице book необходимо скорректировать значение для покупателя в столбце buy таким 
+образом, чтобы оно не превышало количество экземпляров книг, указанных в столбце amount. 
+А цену тех книг, которые покупатель не заказывал, снизить на 10%. */
+UPDATE book SET buy = amount WHERE buy > amount;
+UPDATE book SET price = price * 0.9 WHERE buy = 0;
+
