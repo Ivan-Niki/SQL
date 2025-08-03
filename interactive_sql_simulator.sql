b45ebbaf598a63ecf5dd86c6bf86d5c7964d1035
Ivan-Niki <iFankov@gmail.com>
2025-08-03 23:38:05 +0300

    added task 27 adding records from another table

--- interactive_sql_simulator.sql
+++ interactive_sql_simulator.sql
@@ -269,3 +269,9 @@ VALUES
     ('Белая гвардия', 'Булгаков М.А.', 540.50, 7),
     ('Идиот', 'Достоевский Ф.М.', 360.80, 3);
 	
+
+-- 28) Добавить из таблицы supply в таблицу book, все книги, кроме книг, написанных Булгаковым М.А. и Достоевским Ф.М.
+INSERT INTO book (title, author, price, amount) 
+SELECT title, author, price, amount 
+FROM supply
+WHERE author != 'Булгаков М.А.' AND author != 'Достоевский Ф.М.';
