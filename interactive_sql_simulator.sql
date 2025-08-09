6dda7c4907d08c7b930d624f0f9d7074cd8e7ad9
Ivan-Niki <iFankov@gmail.com>
2025-08-10 00:26:03 +0300

    updated 42th task MONTH operator

--- interactive_sql_simulator.sql
+++ interactive_sql_simulator.sql
@@ -389,5 +389,11 @@ WHERE date_last - date_first IN (
 /* 42) Вывести информацию о командировках, начало и конец которых относятся к одному месяцу (год может быть любой). 
 В результат включить столбцы name, city, date_first, date_last. 
 Строки отсортировать сначала  в алфавитном порядке по названию города, а затем по фамилии сотрудника. */
+SELECT name, city, date_first, date_last
+FROM trip
+WHERE MONTH(date_first) = MONTH(date_last)
+ORDER BY city, name;
+
+
 
 
