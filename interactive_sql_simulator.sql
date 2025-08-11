9fd527bf18f63ddabf8ffa7cba8db1fd7f6dd13c
Ivan-Niki <iFankov@gmail.com>
2025-08-12 01:20:29 +0300

    added 34th task CREATION based on another table

--- interactive_sql_simulator.sql
+++ interactive_sql_simulator.sql
@@ -329,6 +329,20 @@ WHERE author IN (
     HAVING SUM(amount) > 10
     );
 
+/* 34) Создать таблицу заказ (ordering), куда включить авторов и названия тех книг, 
+количество экземпляров которых в таблице book меньше среднего количества экземпляров книг в таблице book. 
+В таблицу включить столбец amount, в котором для всех книг указать одинаковое значение - 
+среднее количество экземпляров книг в таблице book. */
+CREATE TABLE ordering AS
+SELECT author, title, 
+   (
+    SELECT ROUND(AVG(amount))
+    FROM book
+   ) AS amount
+FROM book
+WHERE amount < (SELECT ROUND(AVG(amount)) FROM book);
+
+
 
 
 -- Таблица "Командировки"
