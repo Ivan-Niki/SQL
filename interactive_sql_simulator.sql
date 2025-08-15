765cbf143ab5360ec8cca477d14a9532c412c5ce
Ivan-Niki <iFankov@gmail.com>
2025-08-15 14:01:08 +0300

    added 50th task UPDATE table fine

--- interactive_sql_simulator.sql
+++ interactive_sql_simulator.sql
@@ -500,3 +500,14 @@ FROM fine
 GROUP by name, number_plate, violation
 HAVING count(violation) >= 2
 ORDER by name, number_plate, violation;
+
+/* 50) В таблице fine увеличить в два раза сумму неоплаченных штрафов для отобранных на предыдущем шаге записей. */
+UPDATE fine AS f, (
+	   SELECT name, number_plate, violation
+  		 FROM fine
+ 		GROUP BY name, number_plate, violation
+	   HAVING count(*) >= 2
+	   ) AS dv
+ SET f.sum_fine = f.sum_fine*2
+ WHERE f.date_payment IS NUll
+	   AND (f.name = dv.name AND f.violation = dv.violation);
