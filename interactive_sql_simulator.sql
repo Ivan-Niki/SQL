5ca90009de9fbce64f8ab91efe212147f3ddd06a
Ivan-Niki <iFankov@gmail.com>
2025-08-17 23:54:38 +0300

    added task 51 UPDATE table fine

--- interactive_sql_simulator.sql
+++ interactive_sql_simulator.sql
@@ -511,3 +511,18 @@ UPDATE fine AS f, (
  SET f.sum_fine = f.sum_fine*2
  WHERE f.date_payment IS NUll
 	   AND (f.name = dv.name AND f.violation = dv.violation);
+
+
+/* 51) Водители оплачивают свои штрафы. В таблице payment занесены даты их оплаты. Необходимо:
+-> в таблицу fine занести дату оплаты соответствующего штрафа из таблицы payment; 
+-> уменьшить начисленный штраф в таблице fine в два раза  (только для тех штрафов, 
+информация о которых занесена в таблицу payment) , если оплата произведена не позднее 20 дней со дня нарушения. */
+UPDATE fine f, payment p
+SET f.date_payment = p.date_payment,
+    f.sum_fine = IF(DATEDIFF(f.date_payment, f.date_violation) <= 20, f.sum_fine / 2, f.sum_fine)
+WHERE f.name = p.name AND
+      f.number_plate = p.number_plate AND
+      f.violation = p.violation AND
+      f.date_violation = p.date_violation AND
+      f.date_payment IS NULL;
+	  
