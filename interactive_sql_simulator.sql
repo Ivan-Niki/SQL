9ef8c3ded25f94f28f55b37297950bc2508dff22
Ivan-Niki <iFankov@gmail.com>
2025-08-18 23:41:41 +0300

    added task 52 CREATE new table

--- interactive_sql_simulator.sql
+++ interactive_sql_simulator.sql
@@ -526,3 +526,12 @@ WHERE f.name = p.name AND
       f.date_violation = p.date_violation AND
       f.date_payment IS NULL;
 	  
+
+/* 52) Создать новую таблицу back_payment, куда внести информацию о неоплаченных штрафах 
+(Фамилию и инициалы водителя, номер машины, нарушение, сумму штрафа  и  дату нарушения) из 
+таблицы fine. */
+CREATE TABLE back_payment
+    (SELECT name, number_plate, violation, sum_fine, date_violation
+     FROM fine
+     WHERE date_payment IS NULL);
+
