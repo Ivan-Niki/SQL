c236da63e4df46d1bfc7db5d9df9c273f2904918
Ivan-Niki <iFankov@gmail.com>
2025-08-18 23:42:55 +0300

    added task 53 DELETE from table fine

--- interactive_sql_simulator.sql
+++ interactive_sql_simulator.sql
@@ -535,3 +535,10 @@ CREATE TABLE back_payment
      FROM fine
      WHERE date_payment IS NULL);
 
+
+/* 53) Удалить из таблицы fine информацию о нарушениях, совершенных раньше 1 февраля 2020 года. */
+DELETE FROM fine
+WHERE date_violation < DATE(20200201) AND date_payment IS NOT NULL;
+
+
+
