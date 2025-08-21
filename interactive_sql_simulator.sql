94a0cd93d8e32e4ca1fd93b0445e2a58980a86d6
Ivan-Niki <iFankov@gmail.com>
2025-08-21 23:55:05 +0300

    added 56th task solution FK

--- interactive_sql_simulator.sql
+++ interactive_sql_simulator.sql
@@ -574,6 +574,16 @@ VALUES
 genre_id	| INT PRIMARY KEY AUTO_INCREMENT
 name_genre	| VARCHAR(30)  
 */
+CREATE TABLE book (
+    book_id INT PRIMARY KEY AUTO_INCREMENT, 
+    title VARCHAR(50), 
+    author_id INT NOT NULL,
+    genre_id INT,
+    price DECIMAL(8, 2), 
+    amount INT, 
+    FOREIGN KEY (author_id) REFERENCES author (author_id),
+    FOREIGN KEY (genre_id) REFERENCES genre (genre_id)
+);
 
 
 
