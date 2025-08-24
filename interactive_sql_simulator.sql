f2338c3fe79f81494ef8a713281432fd11bb2457
Ivan-Niki <iFankov@gmail.com>
2025-08-24 23:09:37 +0300

    added 61th task JOIN two tables

--- interactive_sql_simulator.sql
+++ interactive_sql_simulator.sql
@@ -611,3 +611,13 @@ FROM genre AS g
 LEFT JOIN book AS b
     ON g.genre_id = b.genre_id
 WHERE title IS NULL;
+
+/* 61) Вывести информацию о книгах (жанр, книга, автор), относящихся к жанру, включающему слово «роман» в отсортированном по названиям книг виде. */
+SELECT name_genre, title, name_author  
+FROM
+    author 
+    INNER JOIN  book ON author.author_id = book.author_id
+    INNER JOIN genre ON genre.genre_id = book.genre_id
+WHERE name_genre = 'Роман'
+ORDER BY title;
+
