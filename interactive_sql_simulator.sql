d3d2944d91b739010086ccb47f9f8e767bac14d3
Ivan-Niki <iFankov@gmail.com>
2025-08-25 23:41:40 +0300

    added 62th task LEFT JOIN

--- interactive_sql_simulator.sql
+++ interactive_sql_simulator.sql
@@ -621,3 +621,13 @@ FROM
 WHERE name_genre = 'Роман'
 ORDER BY title;
 
+/* 62) Посчитать количество экземпляров  книг каждого автора из таблицы author.  
+Вывести тех авторов, количество книг которых меньше 10, в отсортированном по возрастанию 
+количества виде. Последний столбец назвать Количество. */
+SELECT name_author, sum(amount) AS Количество
+FROM 
+    author LEFT JOIN book
+    on author.author_id = book.author_id
+GROUP BY name_author
+HAVING Количество<10 or Количество is NULL
+ORDER BY Количество ASC;
